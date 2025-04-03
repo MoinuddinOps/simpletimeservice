003f04e85045c9588cc5fba359ea441cfa59370e
moinuddin <moinuddin@bamko.net>
2025-04-03 19:09:50 +0000

    updated the alb_target_group and lambda permissions

--- terraform-serverless/alb_target_group.tf
+++ terraform-serverless/alb_target_group.tf
@@ -1,13 +1,4 @@
 resource "aws_lb_target_group" "lambda_tg" {
   name        = "lambda-target-group"
   target_type = "lambda"
-
-  health_check {
-    enabled             = true
-    interval            = 30
-    timeout             = 5
-    healthy_threshold   = 3
-    unhealthy_threshold = 3
-    matcher             = "200-299"
-  }
 }
--- terraform-serverless/lambda_permissions.tf
+++ terraform-serverless/lambda_permissions.tf
@@ -1,4 +1,5 @@
 resource "aws_lambda_permission" "alb_invoke" {
+  statement_id  = "AllowALBInvoke"
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.my_lambda.function_name
   principal     = "elasticloadbalancing.amazonaws.com"
